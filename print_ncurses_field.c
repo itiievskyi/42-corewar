5225718097898e55ecf58391c400f72111e43e1e
Igor Tiievskyi <it@MacBook-Air-Igor.local>
2018-07-31 16:21:01 +0300

    v.0.25: color highlights for processes added

--- print_ncurses_field.c
+++ print_ncurses_field.c
@@ -26,6 +26,44 @@ static int	get_player(t_ncurse *crwr, t_pc *pc)
 	return (i + 1);
 }
 
+void		check_highlites(t_ncurse *crwr, t_pc *temp, char *cmd)
+{
+	static int	s[4096];
+	int			i;
+	int x;
+	int y;
+
+	x = 4;
+	y = 9;
+	if (ft_strequ(cmd, "write"))
+	{
+		i = (temp->size) % 4096;
+		s[i] = get_player(crwr, temp) * 10;
+	}
+	if (ft_strequ(cmd, "print"))
+	{
+		mvprintw(80, 90, "%.2X ", 0);
+		i = -1;
+		while (++i < 4096)
+		{
+			if (s[i] > 0)
+			{
+				attron(COLOR_PAIR(s[i]) | A_STANDOUT);
+				mvprintw(y + i / 64, (x + (i % 64 * 3)), "%.2X", crwr->tab[i]);
+				attroff(COLOR_PAIR(s[i]) | A_STANDOUT);
+				s[i] *= -1;
+			}
+			else if (s[i] < 0)
+			{
+				attron(COLOR_PAIR(s[i] * (-1)));
+				mvprintw(y + i / 64, (x + (i % 64 * 3)), "%.2X", crwr->tab[i]);
+				attroff(COLOR_PAIR(s[i] * (-1)));
+				s[i] = 0;
+			}
+		}
+	}
+}
+
 void		print_changes(t_ncurse *crwr, t_pc *pc)
 {
 	int i;
@@ -37,9 +75,12 @@ void		print_changes(t_ncurse *crwr, t_pc *pc)
 	temp = pc;
 	x = 4;
 	y = 9;
+	int k = 0;
 	while (temp)
 	{
 		attron(COLOR_PAIR(10 * get_player(crwr, temp)) | A_BOLD);
+		if (temp->size >= 0 && temp->size < 4096)
+			check_highlites(crwr, temp, "write");
 		j = 0;
 		if (temp->change >= 0)
 		{
@@ -51,10 +92,15 @@ void		print_changes(t_ncurse *crwr, t_pc *pc)
 					crwr->tab[i]);
 			}
 		}
+		mvprintw(78 + k, 10, "Player = %d", get_player(crwr, temp));
+		mvprintw(78 + k, 40, "size = %d", temp->size);
+		mvprintw(78 + k, 70, "change = %d", temp->change);
+		k++;
 		temp = temp->next;
 		crwr->proc++;
 		attroff(A_BOLD);
 	}
+	check_highlites(crwr, temp, "print");
 }
 
 void		print_field_start(t_ncurse *crwr, int i, int y, int x)
